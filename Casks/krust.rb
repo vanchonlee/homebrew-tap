0c669db53e2d50ba1aa973ce8ea87f102bc313fe
vanchonlee <vanchonlee@gmail.com>
2026-04-09 00:35:45 +0700

    Release krust v1.3.3

--- Casks/krust.rb
+++ Casks/krust.rb
@@ -1,6 +1,6 @@
 cask "krust" do
-  version "1.3.2"
-  sha256 "00b794b36216b0d2d6d427c75912ea870ae9437322eecb02b0dd86946b027362"
+  version "1.3.3"
+  sha256 "bba5cd40ea515835159603f4ca8329867e927a55fd19b509afbd6f69d97de190"
 
   url "https://github.com/vanchonlee/homebrew-tap/releases/download/v#{version}/krust-#{version}.dmg",
       verified: "github.com/vanchonlee/homebrew-tap/"
